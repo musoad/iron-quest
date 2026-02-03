6cfcfee3da51cf6bb4b78f9b060c49573f2b081c
musoad <musoad92@googlemail.com>
2026-02-03 09:02:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE = "ironquest-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
 
