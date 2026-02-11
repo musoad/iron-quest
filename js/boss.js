ede7964db1ec76ac324b2b218a5f98f6b46ebbc2
musoad <musoad92@googlemail.com>
2026-02-11 15:53:08 +0100

    Update boss.js

--- js/boss.js
+++ js/boss.js
@@ -1,3 +1,37 @@
-// placeholder – wird später gefüllt
-window.IRONQUEST_BOSS = window.IRONQUEST_BOSS || {};
+window.BossSystem = (() => {
 
+const BOSSES = [
+    { week: 2, name: "Foundation Beast", xp: 1000 },
+    { week: 4, name: "Iron Destroyer", xp: 1500 },
+    { week: 8, name: "Overlord", xp: 3000 }
+];
+
+let cleared = JSON.parse(localStorage.getItem("iq_boss") || "{}");
+
+function getCurrentBoss(week) {
+    return BOSSES.find(b => b.week === week);
+}
+
+function clearBoss(week) {
+    const boss = getCurrentBoss(week);
+    if (!boss) return;
+
+    cleared[week] = true;
+    localStorage.setItem("iq_boss", JSON.stringify(cleared));
+
+    if (window.XPSystem) {
+        XPSystem.addXP(boss.xp);
+    }
+}
+
+function isCleared(week) {
+    return !!cleared[week];
+}
+
+return {
+    getCurrentBoss,
+    clearBoss,
+    isCleared
+};
+
+})();
