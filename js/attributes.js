ac7dd806ced079f589d62cbb0f0553880f11f2b1
musoad <musoad92@googlemail.com>
2026-02-16 15:04:29 +0100

    Create attributes.js

---
+++ js/attributes.js
@@ -0,0 +1,8 @@
+(() => {
+  "use strict";
+
+  // Platzhalter (falls du das später für STR/UNI/CORE/END echt nutzt)
+  window.IronQuestAttributes = {
+    list: ["MULTI","UNI","CORE","END"]
+  };
+})();
