1b7de6e1c0975c65de4cb8be41a11eac8712dfd1
musoad <musoad92@googlemail.com>
2026-03-02 10:23:37 +0100

    Update progression.js

--- js/progression.js
+++ js/progression.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const KEY_START = "ironquest_startdate_v5";
+  const KEY_START = "ironquest_startdate_v6";
 
   function getStartDate(){
     let s = localStorage.getItem(KEY_START);
@@ -22,7 +22,7 @@
 
   function xpNeededForNextLevel(level){
     const l = Math.max(1, Number(level||1));
-    return Math.round(350 + 120*l + 32*(l**1.75));
+    return Math.round(380 + 140*l + 34*(l**1.78));
   }
 
   function levelFromTotalXp(totalXp){
