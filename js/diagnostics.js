4eb263ad7c199d35c4fd22d7eb47943e0e0272ca
musoad <musoad92@googlemail.com>
2026-03-03 09:45:34 +0100

    Create diagnostics.js

---
+++ js/diagnostics.js
@@ -0,0 +1,19 @@
+(() => {
+  "use strict";
+  function open(){
+    const msg = [];
+    msg.push("Iron Quest v9.5 Master");
+    msg.push("DB: " + (window.IronDB?.db ? "ready" : "unknown"));
+    msg.push("SW: " + (navigator.serviceWorker?.controller ? "active" : "none"));
+    msg.push("Modules: " + [
+      "XP:"+!!window.IronQuestXP,
+      "Loot:"+!!window.IronQuestLoot,
+      "Equip:"+!!window.IronQuestEquipment,
+      "Gates:"+!!window.IronQuestGates,
+      "Boss:"+!!window.IronQuestBossArena,
+      "Coach:"+!!window.IronQuestCoach
+    ].join("  "));
+    window.UIEffects?.showSystem?.(msg.join("\n"));
+  }
+  window.IronQuestDiagnostics = { open };
+})();
