8207b97736f0ea3d20f2b023594129b22542edfa
musoad <musoad92@googlemail.com>
2026-03-04 08:06:24 +0100

    Update diagnostics.js

--- js/diagnostics.js
+++ js/diagnostics.js
@@ -3,8 +3,8 @@
   function open(){
     const msg = [];
     msg.push("Iron Quest v9.5 Master");
-    msg.push("DB: " + (window.IronDB?.db ? "ready" : "unknown"));
-    msg.push("SW: " + (navigator.serviceWorker?.controller ? "active" : "none"));
+    msg.push("DB: " + ((window.IronDB && window.IronDB.db) ? "ready" : "unknown"));
+    msg.push("SW: " + ((navigator.serviceWorker && navigator.serviceWorker.controller) ? "active" : "none"));
     msg.push("Modules: " + [
       "XP:"+!!window.IronQuestXP,
       "Loot:"+!!window.IronQuestLoot,
@@ -13,7 +13,7 @@
       "Boss:"+!!window.IronQuestBossArena,
       "Coach:"+!!window.IronQuestCoach
     ].join("  "));
-    window.UIEffects?.showSystem?.(msg.join("\n"));
+    (window.UIEffects && (window.UIEffects.showSystem) && window.UIEffects.showSystem)(msg.join("\n"));
   }
   window.IronQuestDiagnostics = { open };
 })();
